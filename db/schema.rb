f025e146fd72bdab8def0b0da6b4122faa0408b2
arthurnfarias <anf2@cin.ufpe.br>
2016-11-09 23:27:21 -0300

    Adição de elementos a User e correção de Views e Gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161108172321) do
+ActiveRecord::Schema.define(version: 20161110020416) do
 
   create_table "krs", force: :cascade do |t|
     t.string   "title"
@@ -51,6 +51,9 @@ ActiveRecord::Schema.define(version: 20161108172321) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.boolean  "admin"
+    t.string   "username"
+    t.string   "first_name"
+    t.string   "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
