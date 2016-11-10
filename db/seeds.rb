1c12fbca828533fa961bee67a9ab2c671bbcb577
arthurnfarias <anf2@cin.ufpe.br>
2016-11-09 23:49:52 -0300

    Adição de User Admin ao seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(username: "arthurnfarias", first_name: "Arthur", last_name: "Farias", email: "anf2@cin.ufpe.br", password: "264895", admin: true)
