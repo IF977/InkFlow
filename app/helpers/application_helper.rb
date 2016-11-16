05e95d8efad450cb00cb041523d97ddf3cc7587c
Caio Cavalcanti <ccat@cin.ufpe.br>
2016-11-16 13:56:20 -0300

    Barra de progresso de OKR adicionada a Index de OKR

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+def progresso_okr (okr)
+        @okr = okr
+        contagem = @okr.krs.count
+        c = 0
+        @okr.krs.each do |i|
+             c = c + i.progress
+        end
+
+        if contagem != 0
+        progresso = c/contagem
+        elsif contagem == 0
+        progresso = 0
+        end
+        progresso
+    end
+
 end
--- app/helpers/krs_helper.rb
+++ app/helpers/krs_helper.rb
@@ -1,2 +1,6 @@
 module KrsHelper
+
+
+
+
 end
