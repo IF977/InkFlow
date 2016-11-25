643b385661a86cfa306f2014ce21c212e0f89104
arthurnfarias <anf2@cin.ufpe.br>
2016-11-25 19:22:18 -0300

    Ajuste nas limitações de acesso e criação do Boolean de editor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,18 @@
 module ApplicationHelper
 
-def progresso_okr (okr)
-        @okr = okr
-        contagem = @okr.krs.count
-        c = 0
-        @okr.krs.each do |i|
-             c = c + i.progress
-        end
+    def progresso_okr (okr)
+            @okr = okr
+            contagem = @okr.krs.count
+            c = 0
+            @okr.krs.each do |i|
+                 c = c + i.progress
+            end
 
-        if contagem != 0
-        progresso = c/contagem
-        elsif contagem == 0
-        progresso = 0
-        end
-        progresso
+            if contagem != 0
+            progresso = c/contagem
+            elsif contagem == 0
+            progresso = 0
+            end
+            progresso
     end
-
 end
